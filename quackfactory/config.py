79f8f3d96d6090f77d56abde0de40619eb4680d7
Kobi <73299267+kobito-kun@users.noreply.github.com>
2020-12-03 18:46:46 -0400

    Update config.py

--- quackfactory/config.py
+++ quackfactory/config.py
@@ -1,9 +1,10 @@
 from discord import Intents
 
 def get_intents() -> Intents:
-  intents = Intents.all()
+  intents = Intents.default()
   intents.reactions = True
   intents.members = True
+  intents.presences = True
   return intents
 
 LOG_FILENAME = 'quack.log'
