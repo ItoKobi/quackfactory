e0d4569e04304030657c27c7eaeed5d97ac532d8
Kobi <73299267+kobito-kun@users.noreply.github.com>
2020-12-03 14:48:39 -0400

    opens a session
    fixed so that it would only message the online rubber ducks. Removed unique instance of a bot and 'before.channel == none' which solves the issue of a user changing from 1 vc to another.

--- quackfactory/cogs/session.py
+++ quackfactory/cogs/session.py
@@ -1,11 +1,4 @@
-from discord.ext import commands
 import discord
-import logging
-
-intents = discord.Intents.default()
-intents.reactions = True
-intents.members = True
-client = discord.Client(intents=intents)
 
 class Session(commands.Cog):
     def __init__(self, bot):
@@ -18,9 +11,9 @@ async def on_voice_state_update(member, before, after):
     role = "Test"
     everyone = "@everyone" 
     channel_id = 756574459556659323
-    message = f"Hey! {member} needs a rubber duck!"
+    message = f"Hey there! {member} needs a rubber duck!"
 
-    if before.channel is None and after.channel.id == channel_id:
+    if after.channel.id == channel_id:
         await after.channel.set_permissions(discord.utils.get(guild.roles, name=role), connect=True)
         await after.channel.set_permissions(discord.utils.get(guild.roles, name=everyone), connect=False)
         await after.channel.edit(user_limit=2)
@@ -28,7 +21,8 @@ async def on_voice_state_update(member, before, after):
         for member in guild.members:
             for role in member.roles:
                 if role.name == "Test": # Rubberduck role idk why but it doesnt work using a variable ;(
-                    await member.send(message)
+                    if str(member.status) == "online" or member.status == discord.Status.online:
+                        await member.send(message)
 
 def setup(bot):
     bot.add_cog(Session(bot))
